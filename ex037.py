ea832bd27ac9d7c069ea8eb8373ac917a64bc717
pewdro <mr.peterfarinha@gmail.com>
2025-08-26 14:39:07 -0300

    Update ex037.py

---
+++ ex037.py
@@ -0,0 +1,10 @@
+num1 = int(input(''))
+num2 = int(input(''))
+
+def op(num1,num2):
+    print (f'{num1} + {num2} = {num1 + num2}')
+    print (f'{num1} - {num2} = {num1 - num2}')
+    print (f'{num1} / {num2} = {num1 / num2}')
+    print (f'{num1} * {num2} = {num1 * num2}')
+
+op(num1,num2)
